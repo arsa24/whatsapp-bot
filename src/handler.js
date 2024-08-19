be704b186c7a99a0d3d4e751c6d9bdcf7282b207
arsa <idunvoneinzbern@gmail.com>
2024-08-19 17:12:07 +0700

    update cli

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,7 @@
+const {exec} = require("child_process")
 const fs = require("fs");
 const path = require("path");
-const { getMessages, simulate, reply, check } = require("./lib");
+const { getMessages, simulate, reply, check, filterMsg } = require("./lib");
 require("../config");
 
 exports.commandHandler = async (sock, msg) => {
@@ -8,6 +9,22 @@ exports.commandHandler = async (sock, msg) => {
     const pathCmd = path.join(__dirname, "commands");
     const message = await getMessages(msg);
     const commands = fs.readdirSync(pathCmd);
+
+    if(check.isOwner(msg) && await message.startsWith("$ ")){
+      const cmd = await filterMsg.position(msg, "except first");
+      try {
+        const output = await new Promise((resolve, reject) => {
+            exec(cmd, async (err, stdout, stderr) => {
+                if(err){reject(new Error(`[ ! ] ${err.message}`))}
+                else if(stderr){reject(new Error(stderr))}
+                else resolve(stdout)
+            });
+        })
+        await reply(sock, msg, output)
+      } catch (e) {
+        await reply(sock, msg, `[ ! ] ${e}`);
+      }
+    }
 
     commands.forEach((cmd) => {
       const commandPath = path.join(pathCmd, cmd);
